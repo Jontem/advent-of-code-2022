cc82566f4ef7e1c036710f745081f6ceed512530
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2022-12-02 17:26:07 +0100

    Day 2

--- Day2/Part1.cs
+++ Day2/Part1.cs
@@ -7,6 +7,37 @@ namespace Day2
 {
     public class Part1
     {
-        public static void Solve() { }
+        // A rock, B paper, C scissors,
+        public static void Solve()
+        {
+            var shapeScore = new Dictionary<string, int> {
+                {"A", 1},
+                {"X", 1},
+                {"B", 2},
+                {"Y", 2},
+                {"C", 3},
+                {"Z", 3},
+
+            };
+            var winScore = new Dictionary<string, int> {
+                {"A X", 3},
+                {"A Y", 6},
+                {"A Z", 0},
+                {"B X", 0},
+                {"B Y", 3},
+                {"B Z", 6},
+                {"C X", 6},
+                {"C Y", 0},
+                {"C Z", 3},
+
+            };
+            var roundScores = File.ReadAllLines("input").Select(x =>
+             {
+                 var myShape = x.Split(" ")[1];
+                 return shapeScore[myShape] + (winScore.ContainsKey(x) ? winScore[x] : 0);
+             });
+
+            Console.WriteLine(roundScores.Sum());
+        }
     }
 }
--- Day2/Part2.cs
+++ Day2/Part2.cs
@@ -7,6 +7,44 @@ namespace Day2
 {
     public class Part2
     {
-        public static void Solve() { }
+        // A rock, B paper, C scissors,
+        public static void Solve()
+        {
+            var shapeScore = new Dictionary<string, int> {
+                {"A", 1},
+                {"X", 1},
+                {"B", 2},
+                {"Y", 2},
+                {"C", 3},
+                {"Z", 3},
+
+            };
+            var winScore = new Dictionary<string, int> {
+                {"X", 0},
+                {"Y", 3},
+                {"Z", 6},
+
+            };
+            var roundScores = File.ReadAllLines("input")
+            .Select(x =>
+             {
+                 var elfShape = x.Split(" ")[0];
+                 var ending = x.Split(" ")[1];
+                 var score = winScore[ending];
+
+                 switch (score)
+                 {
+                     case 3:
+                         return shapeScore[elfShape] + score;
+                     case 6:
+                         return shapeScore[elfShape == "A" ? "B" : elfShape == "B" ? "C" : "A"] + score;
+                     default:
+                         return shapeScore[elfShape == "A" ? "C" : elfShape == "B" ? "A" : "B"] + score;
+                 }
+
+             });
+
+            Console.WriteLine(roundScores.Sum());
+        }
     }
 }
