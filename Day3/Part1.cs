e8401c15ecbdc49938776325791219c3d8b6f400
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2022-12-03 08:53:00 +0100

    Day3

--- Day3/Part1.cs
+++ Day3/Part1.cs
@@ -9,6 +9,32 @@ namespace Day3
     {
         public static void Solve()
         {
+            var priorities = Enumerable.Range(97, 26)
+            .Concat(Enumerable.Range(65, 26))
+            .Select(i => (char)i)
+            .ToList();
+
+            var sums = File.ReadAllLines("input")
+            .Aggregate(new List<int>(), (acc, seed) =>
+            {
+                var c1s = seed.Substring(0, seed.Length / 2)
+                .ToList();
+                var c2s = seed.Substring(seed.Length / 2)
+                .ToList();
+
+                var set = new HashSet<Char>();
+                foreach (var c1 in c1s)
+                {
+                    if (c2s.IndexOf(c1) > -1 && !set.Contains(c1))
+                    {
+                        set.Add(c1);
+                    }
+                }
+                acc.Add(set.Select((c) => priorities.IndexOf(c) + 1).Sum());
+                return acc;
+            });
+
+            Console.WriteLine(sums.Sum());
         }
     }
 }
--- Day3/Part2.cs
+++ Day3/Part2.cs
@@ -9,6 +9,31 @@ namespace Day3
     {
         public static void Solve()
         {
+            var priorities = Enumerable.Range(97, 26)
+            .Concat(Enumerable.Range(65, 26))
+            .Select(i => (char)i)
+            .ToList();
+
+            var sums = File.ReadAllLines("input")
+            .Chunk(3)
+            .Aggregate(new List<int>(), (acc, seed) =>
+            {
+                var elf1 = seed[0].ToList();
+                var elf2 = seed[1].ToList();
+                var elf3 = seed[2].ToList();
+                var set = new HashSet<char>();
+                foreach (var c1 in elf1)
+                {
+                    if (elf2.IndexOf(c1) > -1 && elf3.IndexOf(c1) > -1 && !set.Contains(c1))
+                    {
+                        set.Add(c1);
+                    }
+                }
+                acc.Add(set.Select((c) => priorities.IndexOf(c) + 1).Sum());
+                return acc;
+            });
+
+            Console.WriteLine(sums.Sum());
         }
     }
 }
