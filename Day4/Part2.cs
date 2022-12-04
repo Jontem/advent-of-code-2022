795773bfb193cae5950bb21cbf1dca221c3df127
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2022-12-04 09:00:38 +0100

    Day4

--- Day4/Part2.cs
+++ Day4/Part2.cs
@@ -9,7 +9,29 @@ namespace Day4
     {
         public static void Solve()
         {
+            var overlapping = File.ReadAllLines("input")
+            .Select(line =>
+            {
+                var section = line.Split(",");
+                return (GetPair(section[0]), GetPair(section[1]));
+            })
+            .Where(section =>
+            {
+                var (firstPair, secondPair) = section;
+                return firstPair.Any(fp => secondPair.Contains(fp)) || secondPair.Any(x => firstPair.Contains(x));
+            });
 
+            Console.WriteLine(overlapping.Count());
+        }
+
+        private static List<int> GetPair(string pair)
+        {
+            var split = pair.Split("-");
+            var start = int.Parse(split[0]);
+            var end = int.Parse(split[1]);
+            return Enumerable
+            .Range(start, end - start + 1)
+            .ToList();
         }
     }
 }
