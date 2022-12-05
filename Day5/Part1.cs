a73778308727cb6327c9bba8171c9aa6b596adda
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2022-12-05 16:18:54 +0100

    Day5 skeleton

---
+++ Day5/Part1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    public class Part1
+    {
+        public static void Solve()
+        {
+
+        }
+    }
+}
