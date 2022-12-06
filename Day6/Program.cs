0e07b1180b5f77ed126170b745f0d9de6f0f32db
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2022-12-06 07:11:12 +0100

    Day6

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -11,10 +11,33 @@
 
     public static void Solve1()
     {
+        var markerLength = 4;
+        var input = File.ReadAllText("input");
+        for (var i = 0; i < input.Length; i++)
+        {
+            var marker = input.Substring(i, markerLength);
+            if (marker.ToList().Distinct().Count() == markerLength)
+            {
+                Console.WriteLine(marker);
+                Console.WriteLine(i + markerLength);
+                break;
+            }
+        }
 
     }
     public static void Solve2()
     {
-
+        var markerLength = 14;
+        var input = File.ReadAllText("input");
+        for (var i = 0; i < input.Length; i++)
+        {
+            var marker = input.Substring(i, markerLength);
+            if (marker.ToList().Distinct().Count() == markerLength)
+            {
+                Console.WriteLine(marker);
+                Console.WriteLine(i + markerLength);
+                break;
+            }
+        }
     }
 }
