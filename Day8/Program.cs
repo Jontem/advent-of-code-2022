4cb1987f42acd5a8941c6942b553a6e9c503ffbd
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2022-12-08 18:57:05 +0100

    Day 8 part 1

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -11,9 +11,75 @@
 
     public static void Solve1()
     {
+        var grid = ParseGrid();
+        var visible = new HashSet<Coordinate>();
+        for (var y = 0; y < grid.Count; y++)
+        {
+            for (var x = 0; x < grid[0].Count; x++)
+            {
+                // Console.WriteLine(grid[y][x]);
+                var cord = new Coordinate(x, y);
+                if (IsVisible(grid, cord))
+                {
+                    visible.Add(cord);
+                    // Console.WriteLine($"Visible x={x}, y={y}, value={grid[y][x]}");
+                }
+            }
+        }
+        Console.WriteLine(visible.Count);
     }
 
     public static void Solve2()
     {
     }
+
+    public static List<List<int>> ParseGrid()
+    {
+        return File.ReadAllLines("input")
+        .Select(x => x.ToList().Select(x => int.Parse(x.ToString())).ToList())
+        .ToList();
+    }
+
+    public static bool IsVisible(List<List<int>> grid, Coordinate start)
+    {
+        
+        if (start.Y == 0 || start.Y == grid.Count - 1 || start.X == 0 || start.X == grid[0].Count - 1)
+        {
+            return true;
+        }
+
+        var dirs = new List<Coordinate> {
+            new Coordinate(0, 1),
+            new Coordinate(0, -1),
+            new Coordinate(1, 0),
+            new Coordinate(-1, 0)
+        };
+
+        var curr = grid[start.Y][start.X];
+        foreach (var dir in dirs)
+        {
+            var next = new Coordinate(start.X + dir.X, start.Y + dir.Y);
+            var visible = true;
+            while ((next.Y >= 0 && next.Y < grid.Count) && (next.X >= 0 && next.X < grid[0].Count))
+            {
+                var nextTree = grid[next.Y][next.X];
+                if (nextTree >= curr)
+                {
+                    visible = false;
+                }
+                next = new Coordinate(next.X + dir.X, next.Y + dir.Y);
+            }
+
+            if (visible)
+            {
+                return true;
+            }
+        }
+
+        // Console.WriteLine("hek");
+        // Console.WriteLine(start);
+        return false;
+    }
+
+    public record Coordinate(int X, int Y);
 }
