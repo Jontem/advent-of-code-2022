95ba68941a3727d7eddbaace452d3f733c5863ea
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2022-12-08 17:11:37 +0100

    Day 8 skeleton

---
+++ Day8/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("------------- Part 1 -------------");
+        Solve1();
+        Console.WriteLine("------------- Part 2 -------------");
+        Solve2();
+
+    }
+
+    public static void Solve1()
+    {
+    }
+
+    public static void Solve2()
+    {
+    }
+}
