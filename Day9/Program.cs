384a442be2953c8a2da0b3f719c68cbbf0ce49d9
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2022-12-09 21:10:54 +0100

    Day9 part2

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -49,6 +49,60 @@
 
     public static void Solve2()
     {
+        var head = new Coordinate(0, 0);
+        var tails = Enumerable.Range(0, 9)
+        .Select(_ => new Coordinate(0, 0))
+        .ToList();
+        var tailHist = new List<Coordinate>() { new Coordinate(0, 0) };
+
+        foreach (var line in File.ReadAllLines("input"))
+        {
+            var parts = line.Split(" ");
+            var direction = Directions[parts[0]];
+            var steps = int.Parse(parts[1]);
+
+            for (var i = 1; i <= steps; i++)
+            {
+                head = new Coordinate(head.X + direction.X, head.Y + direction.Y);
+
+                var prevKnot = head;
+                for (var j = 0; j < tails.Count; j++)
+                {
+                    if (Math.Abs(prevKnot.X - tails[j].X) > 1 || Math.Abs(prevKnot.Y - tails[j].Y) > 1)
+                    {
+                        if (prevKnot.X != tails[j].X && prevKnot.Y != tails[j].Y)
+                        {
+                            var moveX = prevKnot.X < tails[j].X ? -1 : 1;
+                            var moveY = prevKnot.Y < tails[j].Y ? -1 : 1;
+                            tails[j] = new Coordinate(tails[j].X + moveX, tails[j].Y + moveY);
+                        }
+                        else if (prevKnot.X != tails[j].X)
+                        {
+                            var moveX = prevKnot.X < tails[j].X ? -1 : 1;
+                            tails[j] = new Coordinate(tails[j].X + moveX, tails[j].Y);
+                        }
+                        else if (prevKnot.Y != tails[j].Y)
+                        {
+                            var moveY = prevKnot.Y < tails[j].Y ? -1 : 1;
+                            tails[j] = new Coordinate(tails[j].X, tails[j].Y + moveY);
+                        }
+                        else
+                        {
+                            throw new Exception("dont go here");
+                        }
+
+                        if (j == 8)
+                        {
+                            tailHist.Add(tails[j]);
+                        }
+                    }
+
+                    prevKnot = tails[j];
+                }
+            }
+        }
+
+        Console.WriteLine(new HashSet<Coordinate>(tailHist).Count);
 
     }
 }
