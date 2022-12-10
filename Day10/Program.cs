a4c6acf373f53561fa8718ec67dcf9e2751ed265
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2022-12-10 08:16:25 +0100

    Day 10 part1

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -8,8 +8,35 @@
         Solve2();
     }
 
+
+
     public static void Solve1()
     {
+        var measureCycle = new HashSet<int> { 20, 60, 100, 140, 180, 220 };
+        var signalStrengths = new List<int>();
+        int cycle = 1, x = 1;
+        foreach (var line in File.ReadAllLines("input"))
+        {
+            var parts = line.Split(" ");
+            var cycleLength = parts.Length > 1 ? 2 : 1;
+            for (var i = 0; i < cycleLength; i++)
+            {
+                cycle++;
+                if (i == 1)
+                {
+                    x += int.Parse(parts[1]);
+                }
+                if (measureCycle.Contains(cycle))
+                {
+                    Console.WriteLine($"Cycle{cycle}: x={x} x*cycle={x * cycle}");
+                    signalStrengths.Add(x * cycle);
+                }
+            }
+
+        }
+        Console.WriteLine("x: " + x);
+        Console.WriteLine("cycle: " + cycle);
+        Console.WriteLine("values: " + signalStrengths.Sum());
     }
 
     public static void Solve2()
