8b0d99704be3b5247e4fce28fc3c858d44ceb6b9
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2022-12-10 09:34:18 +0100

    Day10 part 2

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -41,5 +41,40 @@
 
     public static void Solve2()
     {
+        var measureCycle = new HashSet<int> { 40, 80, 120, 160, 200, 240 };
+        var signalStrengths = new List<int>();
+        int cycle = 0, x = 1;
+        var row = "";
+        foreach (var line in File.ReadAllLines("input"))
+        {
+            var parts = line.Split(" ");
+            var cycleLength = parts.Length > 1 ? 2 : 1;
+            for (var i = 0; i < cycleLength; i++)
+            {
+                row += IsSpriteInSync(cycle % 40, x) ? "#" : ".";
+                cycle++;
+
+                if (i == 1)
+                {
+                    x += int.Parse(parts[1]);
+                }
+
+
+                if (measureCycle.Contains(cycle))
+                {
+                    Console.WriteLine(row);
+                    row = "";
+                }
+            }
+
+        }
+        Console.WriteLine("x: " + x);
+        Console.WriteLine("cycle: " + cycle);
+        Console.WriteLine("values: " + signalStrengths.Sum());
+    }
+
+    static bool IsSpriteInSync(int cycle, int x)
+    {
+        return cycle == x - 1 || cycle == x || cycle == x + 1;
     }
 }
