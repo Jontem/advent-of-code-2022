4760d37337cd427b0aa9e9fd11f3a247a24b78e8
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2022-12-12 21:18:11 +0100

    refactor

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -8,31 +8,25 @@
         Solve2();
     }
 
-    private static Char End = Char.Parse("E");
-    private static Char Start = Char.Parse("S");
-
     public static void Solve1()
     {
-        var grid = File.ReadAllLines("input")
-        .Select(l => l.ToList())
-        .ToList();
+        var grid = File.ReadAllLines("input").Select(l => l.ToList()).ToList();
         Console.WriteLine("Steps: " + BFS(grid, GetStartNode(grid)));
     }
 
     public static void Solve2()
     {
-        var grid = File.ReadAllLines("input")
-        .Select(l => l.ToList())
-        .ToList();
+        var grid = File.ReadAllLines("input").Select(l => l.ToList()).ToList();
 
         var min = int.MaxValue;
         for (var i = 0; i < grid.Count; i++)
         {
             for (var j = 0; j < grid[i].Count; j++)
             {
-                if (GetValue(grid[i][j]) == 'a')
+                var val = grid[i][j];
+                if (val == 'a')
                 {
-                    var steps = BFS(grid, new Node(new Vector2d(j, i), null));
+                    var steps = BFS(grid, new Node(val, new Vector2d(j, i), null));
                     if (steps > 0)
                     {
                         min = Math.Min(min, steps);
@@ -44,6 +38,9 @@
         Console.WriteLine("Steps: " + min);
     }
 
+    private static Char End = Char.Parse("E");
+    private static Char Start = Char.Parse("S");
+
     private static int BFS(List<List<char>> grid, Node start)
     {
         var queue = new Queue<Node>(new List<Node> { start });
@@ -51,34 +48,30 @@
         while (queue.Count > 0)
         {
             var currNode = queue.Dequeue();
-            var currValue = GetValue(grid[currNode.Pos.Y][currNode.Pos.X]);
-            if (grid[currNode.Pos.Y][currNode.Pos.X] == End)
+            if (currNode.Value == End)
             {
                 var val = CalculatePath(grid, currNode);
                 return val;
             }
-            var neighbours = GetAdjacents(grid, currNode.Pos);
+            var neighbours = GetNeighbours(grid, currNode.Pos);
             foreach (var neighbourPos in neighbours)
             {
-                var neighbourValue = GetValue(grid[neighbourPos.Y][neighbourPos.X]);
                 if (visited.Contains(neighbourPos))
                 {
                     continue;
                 }
 
-                if (neighbourValue <= currValue + 1)
+                if (GetRealValue(grid[neighbourPos.Y][neighbourPos.X]) <= GetRealValue(currNode.Value) + 1)
                 {
-                    queue.Enqueue(new Node(neighbourPos, currNode));
+                    queue.Enqueue(new Node(grid[neighbourPos.Y][neighbourPos.X], neighbourPos, currNode));
                     visited.Add(neighbourPos);
                 }
             }
-
         }
-
         return -1;
     }
 
-    public record Node(Vector2d Pos, Node? Parent);
+    public record Node(Char Value, Vector2d Pos, Node? Parent);
     public record Vector2d(int X, int Y);
 
     private static List<Vector2d> Directions = new List<Vector2d> {
@@ -88,7 +81,7 @@
         new Vector2d(0, 1),
     };
 
-    private static Char GetValue(Char value)
+    private static Char GetRealValue(Char value)
     {
         return value == Start ? 'a' : value == End ? 'z' : value;
     }
@@ -101,7 +94,7 @@
             {
                 if (grid[i][j] == Start)
                 {
-                    return new Node(new Vector2d(j, i), null);
+                    return new Node(grid[i][j], new Vector2d(j, i), null);
                 }
             }
 
@@ -110,7 +103,7 @@
         throw new Exception("Couldn't find start node");
     }
 
-    private static List<Vector2d> GetAdjacents(List<List<Char>> grid, Vector2d current)
+    private static List<Vector2d> GetNeighbours(List<List<Char>> grid, Vector2d current)
     {
         var adjacents = new List<Vector2d>();
         foreach (var dir in Directions)
